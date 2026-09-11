411491e0687577dd1dd831c95a54a86273f90a0a
ok2468 <c.seireythyda@zispp.org>
2026-09-11 16:30:49 +0700

    Add files via upload

--- data.js
+++ data.js
@@ -16,6 +16,7 @@ const CARS = [
         Rating: 70
     },
     {
+        id: 3,
         Title: "Maserati",
         Description: "Maserati is a luxury Italian automotive manufacturer known for building high-performance sports cars, sedans, and SUVs",
         Price: "280000",
@@ -25,6 +26,7 @@ const CARS = [
 
 
     {
+        id: 4,
         Title: "Bentley",
         Description: "Bentley Motors Limited is a British manufacturer of luxury cars and SUVs headquartered in Crewe, England ",
         Price: "380000",
@@ -32,11 +34,52 @@ const CARS = [
         Rating: 95
     },
     {
+        id: 5,
         Title: "BMW",
         Description: "BMW is a major German Official BMW Website company that builds luxury cars, sports vehicles, and motorcycles ",
         Price: "140000",
         Image: "https://www.bmw-m.com/content/dam/bmw/marketBMW_M/www_bmw-m_com/all-models/model-navigation/bmw-x6-m-competition-flyout1.png",
         Rating: 97
+    },
+       {
+        id: 6,
+        Title: "Daewook",
+        Description: " was a major South Korean automotive brand that was acquired by General Motors in 2002 and eventually rebranded into GM Korea, with its vehicle lineup shifted to Chevrolet",
+        Image: "https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1500w,f_auto,q_auto:best/msnbc/Components/Photos/050112/050112_matiz_hmed.jpg",
+        Price: "30000",
+        Rating: 74
+    },
+       {
+        id: 7,
+        Title: "Mclaren",
+        Description: "BMW is a major German Official BMW Website company that builds luxury cars, sports vehicles, and motorcycles ",
+        Price: "140000",
+        Image: "https://cars-assets-production.mclaren.com/1410/conversions/mclaren_automotive_750s_spider_front_right_profile-1000.png",
+        Price: "320000",
+        Rating: 90
+    },
+       {
+        id: 8,
+        Title: "Chrysler",
+        Description: "Chrysler is an American automobile brand owned by Stellantis that focuses on upscale family vehicles and innovative minivans  ",
+        Price: "57000",
+        Image: "https://platform.cstatic-images.com/in/v2/stock_photos/51b1ffba-acda-47ea-966f-a615b69b5d5b/0ad75471-b962-4eaf-9aaf-d8ff789ee4bc.png",
+        Rating: 78                  
+    },   {
+        id: 9,
+        Title: "Jaguar",
+        Description: "Jaguar is a British luxury and sports car brand known for sleek designs, powerful performance, and a rich racing heritage",
+        Price: "79900",
+        Image: "https://cars.usnews.com/static/images/Auto/izmo/i159615080/2022_jaguar_f_type_angularfront.jpg",
+        Rating: 80
+    },
+       {
+        id: 10,
+        Title: "Chevrolet",
+        Description: "Chevrolet, commonly known as Chevy, is a major American automobile manufacturer founded in 1911 and operated as a division of General Motors  ",
+        Price: "155000",
+        Image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdNu8WphvetDOdp6fn1qFSTPzFXpjlHRnQdqtxtHxJbg&s=10 ",
+        Rating: 80
     },
 ]
 
