31e9a63c6b88721286221eaab0b53cbff3d94b59
ok2468 <c.seireythyda@zispp.org>
2026-09-14 16:18:16 +0700

    Add files via upload

--- data.js
+++ data.js
@@ -1,27 +1,33 @@
 const CARS = [
     {
         id: 1,
-        Title: "Volkswagen beetle",
+        Title: "Volkswagen",
         Description: "The Volkswagen Beetle is a two-door, rear-engine economy car that became one of the most famous and bestselling vehicles in automotive history",
         Price: "30000",
-        Image: "./car_1.jpg",
-        Rating: 89
+        Image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkbo9Fy7FPEqgER9WUgTlCqRMus7h-exCFMy-zHPQ5JA&s=10",
+        Rating: 89,
+        Year: 1934,
+        Model: "Volkswagen Beetle"
     },
     {
         id: 2,
-        Title: "Mazda Miata",
+        Title: "Mazda",
         Description: "The Mazda MX-5 Miata is a lightweight, rear-wheel-drive, two-seater sports car known for its affordable fun and agile handling)",
         Price: "38000",
-        Image: "./car_2.jpg",
-        Rating: 70
+        Image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWa07J6kVQ8q7hZebh11cUBnwcLyCYgFxWxNRXF8szIQ&s=10",
+        Rating: 70,
+        Year:   1989,
+        Model: "Mazda Miata MX-5"
     },
-    {
+    {   
         id: 3,
         Title: "Maserati",
         Description: "Maserati is a luxury Italian automotive manufacturer known for building high-performance sports cars, sedans, and SUVs",
         Price: "280000",
-        Image: "./car_3.avif",
-        Rating: 80
+        Image: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSCHTBY8T_9SEztwLwLVZN-aoC1durNlvRMxTgCjx3S7Rhtv_DKpvznq2iOw-E",
+        Rating: 80,
+        Year: 1914,
+        Model: "MC20 Cielo Maserati"
     },
 
 
@@ -31,7 +37,9 @@ const CARS = [
         Description: "Bentley Motors Limited is a British manufacturer of luxury cars and SUVs headquartered in Crewe, England ",
         Price: "380000",
         Image: "https://s1.cdn.autoevolution.com/images/models/BENTLEY_Flying-Spur-2026_main.jpg",
-        Rating: 95
+        Rating: 95,
+        Year: 1919,
+        Model: "Flying Spur Bentley"
     },
     {
         id: 5,
@@ -39,7 +47,9 @@ const CARS = [
         Description: "BMW is a major German Official BMW Website company that builds luxury cars, sports vehicles, and motorcycles ",
         Price: "140000",
         Image: "https://www.bmw-m.com/content/dam/bmw/marketBMW_M/www_bmw-m_com/all-models/model-navigation/bmw-x6-m-competition-flyout1.png",
-        Rating: 97
+        Rating: 97,
+        Year: 1916,
+        Model: "BMW X6"
     },
     {
         id: 6,
@@ -47,7 +57,9 @@ const CARS = [
         Description: " was a major South Korean automotive brand that was acquired by General Motors in 2002 and eventually rebranded into GM Korea, with its vehicle lineup shifted to Chevrolet",
         Image: "https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1500w,f_auto,q_auto:best/msnbc/Components/Photos/050112/050112_matiz_hmed.jpg",
         Price: "30000",
-        Rating: 74
+        Rating: 74,
+        Year: 1983,
+        Model: "Gentra X Daewook"
     },
     {
         id: 7,
@@ -55,7 +67,10 @@ const CARS = [
         Description: "BMW is a major German Official BMW Website company that builds luxury cars, sports vehicles, and motorcycles ",
         Price: "320000",
         Image: "https://cars-assets-production.mclaren.com/1410/conversions/mclaren_automotive_750s_spider_front_right_profile-1000.png",
-        Rating: 90
+        Rating: 90,
+        Year: 1964,
+        Model: "GTS Mclaren"
+
     },
     {
         id: 8,
@@ -63,7 +78,9 @@ const CARS = [
         Description: "Chrysler is an American automobile brand owned by Stellantis that focuses on upscale family vehicles and innovative minivans  ",
         Price: "57000",
         Image: "https://platform.cstatic-images.com/in/v2/stock_photos/51b1ffba-acda-47ea-966f-a615b69b5d5b/0ad75471-b962-4eaf-9aaf-d8ff789ee4bc.png",
-        Rating: 78
+        Rating: 78,
+        Year: 1957,
+        Model: "Chryslar 300C"
     }, {
         id: 9,
         Title: "Jaguar",
