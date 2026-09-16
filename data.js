525c530bff5d1fb8e2d4397c92966f57974e69f7
sethisakngounskn-tech <sethisakngoun.skn@gmail.com>
2026-09-16 13:33:22 +0700

    Add files via upload

--- data.js
+++ data.js
@@ -1,105 +1,105 @@
 const CARS = [
     {
         id: 1,
-        Title: "Volkswagen",
-        Description: "The Volkswagen Beetle is a two-door, rear-engine economy car that became one of the most famous and bestselling vehicles in automotive history",
-        Price: "30000",
-        Image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkbo9Fy7FPEqgER9WUgTlCqRMus7h-exCFMy-zHPQ5JA&s=10",
-        Rating: 89,
-        Year: 1934,
-        Model: "Volkswagen Beetle"
+        title: "Volkswagen",
+        description: "The Volkswagen Beetle is a two-door, rear-engine economy car that became one of the most famous and bestselling vehicles in automotive history",
+        price: "30000",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkbo9Fy7FPEqgER9WUgTlCqRMus7h-exCFMy-zHPQ5JA&s=10",
+        rating: 89,
+        year: 1934,
+        model: "Volkswagen Beetle"
     },
     {
         id: 2,
-        Title: "Mazda",
-        Description: "The Mazda MX-5 Miata is a lightweight, rear-wheel-drive, two-seater sports car known for its affordable fun and agile handling)",
-        Price: "38000",
-        Image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWa07J6kVQ8q7hZebh11cUBnwcLyCYgFxWxNRXF8szIQ&s=10",
-        Rating: 70,
-        Year:   1989,
-        Model: "Mazda Miata MX-5"
+        title: "Mazda",
+        description: "The Mazda MX-5 Miata is a lightweight, rear-wheel-drive, two-seater sports car known for its affordable fun and agile handling)",
+        price: "38000",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWa07J6kVQ8q7hZebh11cUBnwcLyCYgFxWxNRXF8szIQ&s=10",
+        rating: 70,
+        year:   1989,
+        model: "Mazda Miata MX-5"
     },
     {   
         id: 3,
-        Title: "Maserati",
-        Description: "Maserati is a luxury Italian automotive manufacturer known for building high-performance sports cars, sedans, and SUVs",
-        Price: "280000",
-        Image: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSCHTBY8T_9SEztwLwLVZN-aoC1durNlvRMxTgCjx3S7Rhtv_DKpvznq2iOw-E",
-        Rating: 80,
-        Year: 1914,
-        Model: "MC20 Cielo Maserati"
+        title: "Maserati",
+        description: "Maserati is a luxury Italian automotive manufacturer known for building high-performance sports cars, sedans, and SUVs",
+        price: "280000",
+        image: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSCHTBY8T_9SEztwLwLVZN-aoC1durNlvRMxTgCjx3S7Rhtv_DKpvznq2iOw-E",
+        rating: 80,
+        year: 1914,
+        model: "MC20 Cielo Maserati"
     },
 
 
     {
         id: 4,
-        Title: "Bentley",
-        Description: "Bentley Motors Limited is a British manufacturer of luxury cars and SUVs headquartered in Crewe, England ",
-        Price: "380000",
-        Image: "https://s1.cdn.autoevolution.com/images/models/BENTLEY_Flying-Spur-2026_main.jpg",
-        Rating: 95,
-        Year: 1919,
-        Model: "Flying Spur Bentley"
+        title: "Bentley",
+        description: "Bentley Motors Limited is a British manufacturer of luxury cars and SUVs headquartered in Crewe, England ",
+        price: "380000",
+        image: "https://s1.cdn.autoevolution.com/images/models/BENTLEY_Flying-Spur-2026_main.jpg",
+        rating: 95,
+        year: 1919,
+        model: "Flying Spur Bentley"
     },
     {
         id: 5,
-        Title: "BMW",
-        Description: "BMW is a major German Official BMW Website company that builds luxury cars, sports vehicles, and motorcycles ",
-        Price: "140000",
-        Image: "https://www.bmw-m.com/content/dam/bmw/marketBMW_M/www_bmw-m_com/all-models/model-navigation/bmw-x6-m-competition-flyout1.png",
-        Rating: 97,
-        Year: 1916,
-        Model: "BMW X6"
+        title: "BMW",
+        description: "BMW is a major German Official BMW Website company that builds luxury cars, sports vehicles, and motorcycles ",
+        price: "140000",
+        image: "car_5.webp",
+        rating: 97,
+        year: 1916,
+        model: "BMW X6"
     },
     {
         id: 6,
-        Title: "Daewook",
-        Description: " was a major South Korean automotive brand that was acquired by General Motors in 2002 and eventually rebranded into GM Korea, with its vehicle lineup shifted to Chevrolet",
-        Image: "https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1500w,f_auto,q_auto:best/msnbc/Components/Photos/050112/050112_matiz_hmed.jpg",
-        Price: "30000",
-        Rating: 74,
-        Year: 1983,
-        Model: "Gentra X Daewook"
+        title: "Daewook",
+        description: " was a major South Korean automotive brand that was acquired by General Motors in 2002 and eventually rebranded into GM Korea, with its vehicle lineup shifted to Chevrolet",
+        image: "https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1500w,f_auto,q_auto:best/msnbc/Components/Photos/050112/050112_matiz_hmed.jpg",
+        price: "30000",
+        rating: 74,
+        year: 1983,
+        model: "Gentra X Daewook"
     },
     {
         id: 7,
-        Title: "Mclaren",
-        Description: "BMW is a major German Official BMW Website company that builds luxury cars, sports vehicles, and motorcycles ",
-        Price: "320000",
-        Image: "https://cars-assets-production.mclaren.com/1410/conversions/mclaren_automotive_750s_spider_front_right_profile-1000.png",
-        Rating: 90,
-        Year: 1964,
-        Model: "GTS Mclaren"
+        title: "Mclaren",
+        description: "BMW is a major German Official BMW Website company that builds luxury cars, sports vehicles, and motorcycles ",
+        price: "320000",
+        image: "car_7.jpeg",
+        rating: 90,
+        year: 1964,
+        model: "GTS Mclaren"
 
     },
     {
         id: 8,
-        Title: "Chrysler",
-        Description: "Chrysler is an American automobile brand owned by Stellantis that focuses on upscale family vehicles and innovative minivans  ",
-        Price: "57000",
-        Image: "https://platform.cstatic-images.com/in/v2/stock_photos/51b1ffba-acda-47ea-966f-a615b69b5d5b/0ad75471-b962-4eaf-9aaf-d8ff789ee4bc.png",
-        Rating: 78,
-        Year: 1957,
-        Model: "Chryslar 300C"
+        title: "Chrysler",
+        description: "Chrysler is an American automobile brand owned by Stellantis that focuses on upscale family vehicles and innovative minivans  ",
+        price: "57000",
+        image: "car_8.webp",
+        rating: 78,
+        year: 1957,
+        model: "Chryslar 300C"
     }, {
         id: 9,
-        Title: "Jaguar",
-        Description: "Jaguar is a British luxury and sports car brand known for sleek designs, powerful performance, and a rich racing heritage",
-        Price: "79900",
-        Image: "https://cars.usnews.com/static/images/Auto/izmo/i159615080/2022_jaguar_f_type_angularfront.jpg",
-        Rating: 80,
-        Year: 1935,
-        Model: "F-type Jaguar"
+        title: "Jaguar",
+        description: "Jaguar is a British luxury and sports car brand known for sleek designs, powerful performance, and a rich racing heritage",
+        price: "79900",
+        image: "https://cars.usnews.com/static/images/Auto/izmo/i159615080/2022_jaguar_f_type_angularfront.jpg",
+        rating: 80,
+        year: 1935,
+        model: "F-type Jaguar"
     },
     {
         id: 10,
-        Title: "Chevrolet",
-        Description: "Chevrolet, commonly known as Chevy, is a major American automobile manufacturer founded in 1911 and operated as a division of General Motors  ",
-        Price: "155000",
-        Image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdNu8WphvetDOdp6fn1qFSTPzFXpjlHRnQdqtxtHxJbg&s=10 ",
-        Rating: 80,
-        Year: 1913,
-        Model: "Chevrolet Suburban"
+        title: "Chevrolet",
+        description: "Chevrolet, commonly known as Chevy, is a major American automobile manufacturer founded in 1911 and operated as a division of General Motors  ",
+        price: "155000",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdNu8WphvetDOdp6fn1qFSTPzFXpjlHRnQdqtxtHxJbg&s=10 ",
+        rating: 80,
+        year: 1913,
+        model: "Chevrolet Suburban"
     },
 ]
 
--- script.js
+++ script.js
@@ -1,52 +1,255 @@
-console.log("everything is ready")
+import { CARS } from "./data.js"
 
-const inputValue = document.querySelector("#inputValue")
-const filter = document.querySelector("#filter")
-const resetBtn = document.querySelector("#resetBtn")
-const container = document.querySelector(".container")
+window.addEventListener('DOMContentLoaded', main)
+
+let index = 0
+let timer = null
+
+function main() {
+
+  handleSearch()
+  sort()
+  carTag()
+  renderCars(CARS)
+  // animateSlideShow()
+  timer = setInterval(animateSlideShow, 1000)
+  modal()
+}
+
+function modal() {
+  const carsWrapper = document.querySelector(".cars-wrapper")
+
+  carsWrapper.addEventListener("click", (e) => {
+    const detailBtn = e.target.closest(".detail-btn")
+
+    if (!detailBtn) return
+
+    openModal(detailBtn.dataset.index)
+    HandleTab()
+  })
+
+  document.addEventListener("click", (e) => {
+    if (e.target.matches("#closeBtn")) {
+      closeModal()
+    }
+  })
+
+
+}
+
+
+function getCarCard(cars) {
+
+  return `
+    <div class="card">
+      <div class="car-img">
+        <img src="${cars.image}" alt="">
+      </div>
+
+      <div class="car-detail">
+        <div class="progressbar">
+          <div class="progressbox" style="width: ${cars.rating}%;">
+            <span class="progressvalue">${cars.rating}%</span>
+          </div>
+        </div>
+        <h4 class="car-model">$${cars.price}.00</h4>
+        <h4 class="car-model">${cars.model}</h4>
+        <button class="detail-btn" data-index="${cars.id}">Show Detail</button>
+      </div>
+    </div>
+    `
+}
+
+function renderCars(cars) {
+  const wrapper = document.querySelector(".cars-wrapper")
+  wrapper.innerHTML = ``
+
+
+  for (let i = 0; i < cars.length; i++) {
+    wrapper.innerHTML += getCarCard(cars[i])
+  }
+}
+
+function handleSearch() {
+
+  const carInput = document.querySelector("#searchCar")
+  const form = document.querySelector("form")
+
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    let searchTerm = carInput.value.toLowerCase()
+    console.log(searchTerm)
+
+    let matchCars = CARS.filter((object) =>
+      object.title.includes(searchTerm) || object.model.toLowerCase().includes(searchTerm))
+    console.log(matchCars)
+
+    renderCars(matchCars)
+  })
+
+
+}
+
+
+function sort() {
+
+  const priceBtn = document.querySelector("#priceBtn")
+
+  let priceTick = 0
+  priceBtn.addEventListener("click", () => {
+    priceTick += 1
+    if (priceTick % 2 != 0) {
+      const sortByPrice = CARS.sort((objA, objB) => objA.price - objB.price)
+      renderCars(sortByPrice)
+    } else if (priceTick % 2 == 0) {
+      const sortByPrice = CARS.sort((objA, objB) => objB.price - objA.price)
+      renderCars(sortByPrice)
+    }
+    
+  })
+
+  const ratingBtn = document.querySelector("#ratingBtn")
+
+  let ratingTick = 0
+  ratingBtn.addEventListener("click", () => {
+    ratingTick += 1
+    if (ratingTick % 2 != 0) {
+      const sortByRating = CARS.sort((objA, objB) => objA.rating - objB.rating)
+      renderCars(sortByRating)
+    } else if (ratingTick % 2 == 0) {
+      const sortByRating = CARS.sort((objA, objB) => objB.rating - objA.rating)
+      renderCars(sortByRating)
+    }
+  })
+
+
+  const recBtn = document.querySelector("#recBtn")
+
+  recBtn.addEventListener("click", () => {
+    randomCar()
+  })
+
+}
 
 
 
-inputValue.addEventListener("change", () => {
-    let numbers = parseInt(inputValue.value)
+function animateSlideShow() {
+  const slides = document.querySelectorAll(".slide")
+  // console.log(slides)
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none"
+  }
+  slides[index].style.display = "block"
+  index += 1
+  if (index > slides.length - 1) {
+    index = 0
+  }
+}
+
+function openModal(id) {
+
+  let carobject = CARS.find((object) => object.id == id)
+  const modalWrapper = document.querySelector(".modal-wrapper")
+
+  modalWrapper.classList.remove("hide")
+  console.log(modalWrapper)
+
+  modalWrapper.innerHTML = `
+  <div class="modal-container" style="background-image: url(${carobject.image});">
+      <div class="modal-overlay">
+        <div class="modal-head">
+          <h4>${carobject.model}</h4>
+          <button id="closeBtn" aria-label="Close details">X</button>
+        </div>
+      <section id="tabbar">
+
+        <div class="btn-group">
+          <button class="tab-btn active" data-page="description">DESCRIPTION</button>
+          <button class="tab-btn" data-page="reviews">REVIEWS</button>
+          <button class="tab-btn" data-page="contact">CONTACT</button>
+        </div>
 
 
-    for (let i = 1; i <= numbers; i++) {
+        <div class="content-wrapper">
 
-        if (i % 2 == 0) {
-        container.innerHTML += `
-            <div class="number even">${i}</div>
-        `
+          <div class="content active" id="description">
+            ${carobject.description}
+          </div>
+
+          <div class="content" id="reviews">
+            Overall rating: ${carobject.rating}
+          </div>
+
+          <div class="content" id="contact">
+            Company: ${carobject.title}
+          </div>
+
+        </div>
+      </section>
         
-        } else {
-            container.innerHTML += `
-                <div class="number odd">${i}</div>
-            `
+
+          
+
+        
+      </div>
+  </div>`
+
+}
+
+function closeModal() {
+  const modalWrapper = document.querySelector(".modal-wrapper")
+  modalWrapper.classList.add("hide")
+}
+
+function HandleTab() {
+  const tabbar  = document.querySelector("#tabbar")
+const tabbtn = document.querySelectorAll(".tab-btn")
+const contents = document.querySelectorAll(".content")
+
+tabbar.addEventListener("click", (e) => {
+
+    let target = e.target
+    console.log(target)
+    let tabname = target.dataset.page
+    console.log(tabname)
+
+    if (tabname) {
+    
+        for (let i = 0; i < tabbtn.length; i++) {
+            if (tabbtn[i].classList.contains("active")) {
+                tabbtn[i].classList.remove("active")
+                contents[i].style.display = "none"
+            
+            }
         }
+
+
+        target.classList.add("active")
+        const targetContent = document.querySelector("#"+tabname)
+        targetContent.style.display = "block"
 
     }
 
-})
-const allNumbers = document.querySelectorAll(".number")
-
-filter.addEventListener("change", () => {
-    
-    let selected = filter.value
-
-    const allNumbers = document.querySelectorAll(".number")
-    console.log(allNumbers)
-
-    for (let i = 0; i < allNumbers.length; i++) {
-        let number = allNumbers[i]
-
-        if (number.classList.contains(selected)) {
-            number.style.opacity = 1
-        } else {
-            number.style.opacity = 0.2
-        }
-    }
-    
-    
 
 })
+}
 
+function randomCar() {
+  let rec = 0
+  rec = Math.floor(Math.random() * CARS.length)
+  let randomValue = CARS[rec]
+  console.log(randomValue)
+  renderCars([randomValue]) 
+}
+
+function carTag() {
+  const card = document.querySelectorAll(".card")
+  if (CARS.price < 100000) {
+    card.style.backgroundColor = "green"
+  } else if (CARS.price > 100000 && CARS.price < 200000) {
+    card.style.backgroundColor = "yellow"
+  } else if (CARS.price > 200000 && CARS.price < 300000) {
+    card.style.backgroundColor = "red"
+  }
+}
